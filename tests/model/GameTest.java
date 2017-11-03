9e6f26b182a42626db2e5cf1d5d5bd9fdf9438d5
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-11-03 13:27:34 +0100

    RED - Game - board getter

--- tests/model/GameTest.java
+++ tests/model/GameTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GameTest
 {
@@ -24,6 +26,20 @@ class GameTest
         sut.startNewGame();
         
         Mockito.verify(bf).getKalahaBoard();
+    }
+    
+    @Test
+    void GameTest_getBoard_shouldReturnBoard()
+    {
+      
+        BoardFactory bf = mock(BoardFactory.class);
+        when(bf.getKalahaBoard()).thenReturn(new Board());
+        Game sut = new Game(bf);
+        sut.startNewGame();
+        
+        Board actual = sut.getBoard();
+        
+        assertEquals(actual.getClass(), actual.getClass());
     }
     
     
