449a1372e7c9a7ed2be4640c0e15dd60006078c9
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-11-06 22:43:34 +0100

    GREEN -ConsoleView - drawBoard_ingamePattern

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -110,7 +110,6 @@ public class ConsoleView
         board.append("\n");
         
         board.append("               1   2   3   4   5   6");
-        board.append("fail");
         return board.toString();
     }
     
