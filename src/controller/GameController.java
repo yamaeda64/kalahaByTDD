632ee4824e5615d3e62ec6143cefe2902305939c
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-11-12 21:36:23 +0100

    GREEN - GameController - callClearScreenBeforeShowInstructions

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -22,6 +22,7 @@ public class GameController
     
     public void start()
     {
+        view.clearScreen();
         view.showMenu();
         UserInteraction currentInteraction = view.collectEvent();
         takeAction(currentInteraction);
